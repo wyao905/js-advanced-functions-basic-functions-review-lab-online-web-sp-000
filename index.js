d8fcc759d5a84238ac5acd18c14d56c795e61cdf
Warren Yao <warrenyca905@gmail.com>
2020-01-27 16:20:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}`
+}
+
+mondayWork = function(work = "go to the office") {
+  return `This Monday, I will ${work}`
+}
